7828ebf2b9c737d30ea94054bd507bbd53de9952
AdamPaslawski <adampaslawski@gmail.com>
2021-01-25 14:44:27 -0500

    edits to home page words

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 50,
+      backSpeed: 25,
       backDelay: 2000
     });
   }
